1d4a72a67072fc64aa7df58f427be27789064a94
mlitman <awesomefat@gmail.com>
2020-09-15 11:59:32 -0500

    solution for HW3

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 
 public class Driver 
 {
 	public static void main(String[] args)
 	{
-		System.out.println("hello world");
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter a number: ");
+		String numberAsAString = input.nextLine();
+		int numberAsAnInt = Integer.parseInt(numberAsAString);
+		String answer = "";
+		while(numberAsAnInt != 0)
+		{
+			answer = (numberAsAnInt % 2) + answer;
+			numberAsAnInt = numberAsAnInt / 2;
+		}
+		System.out.println(answer);
 	}
 }
